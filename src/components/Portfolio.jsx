ffc298c1dd310f8bd67127837faba6da73a63217
Ian <yli031118@gmail.com>
2023-08-10 18:31:09 +0800

    add title for projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,16 +5,21 @@ import Title from "./Title";
 
 function Portfolio() {
     return (
-        <div className = "flex flex-col space-y-4 md:flex-row items-center justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {portfolio.map(project => (
-                    <PortfolioItem 
-                    imgUrl={project.imgUrl}
-                    title={project.title}
-                    stack={project.stack}
-                    link={project.link}
-                    />
-                ))}
+        <div className="flex flex-col md:flex-row justify-center">
+            <div className="w-full md:w-7/12">
+            <Title>Projects</Title>
+                <div className = "flex flex-col space-y-4 md:flex-row items-center justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {portfolio.map(project => (
+                            <PortfolioItem 
+                            imgUrl={project.imgUrl}
+                            title={project.title}
+                            stack={project.stack}
+                            link={project.link}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
