c32cfabb1693a420a99c5e7824795c1f1645a331
Ian <yli031118@gmail.com>
2024-03-01 11:27:25 -0800

    update timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,22 +2,22 @@ export default [
     {
         title: 'AMISTAD Lab',
         duration: 'May 2023 - current',
-        details: 'Undergraduate Researcher'
+        details: 'Undergraduate Researcher - Formalized abductive reasoning through Bayesian methods,  demonstrated the probabilistic bounds of uncertainty in abductive inferences, and integrated an information-theoretic perspective to connect abduction with machine learning principles.'
     },
     {
         title: 'P-ai',
         duration: 'Feb. 2023 - current',
-        details: "Project Manager"
+        details: "Project Manager - Led the development of a convolutional neural network for stock classification"
     },
     {
         title: 'DAISION',
         duration: 'Jan. 2023 - May 2023',
-        details: "Software Engineering Intern"
+        details: "Software Engineering Intern - Contributed to the development of a medical document classification system to streamline data processing, facilitated the containerization of application components for easier deployment, and researched automatic patient diagnosis leveraging reinforcement learning techniques and the MIMIC-III dataset."
     },
     {
         title: 'Backgammon Research Group',
         duration: 'Nov. 2021 - Jun 2022',
-        details: "Research Assisstant"
+        details: "Research Assisstant - Collaborated with Professor Arthur Benjamin to devise optimal backgammon strategies, analyzing over 40 million board positions for data-driven insights and significantly improving betting accuracy."
     },
     {
         title: "Harvey Mudd College",
